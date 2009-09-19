1975d6b9afae16f2ebbd9d7c0aa0b77211903e65
vero13 <vero13@c73f237e-9c17-11de-bb5f-cdebcd2927f5>
2009-09-19 02:05:08 +0000

    Clase util, hace falta que se los explique... mire a 55 minutos de mi cumple estoy programando jajaja

---
+++ Equinoccio/Util/Util.cpp
@@ -0,0 +1,15 @@
+#include "Util.h"
+
+/****************************************************************************/
+std::string Util::intToString(uint32_t numero) {
+	
+	std::string snumero;
+	std::stringstream cvz;
+	cvz.width();
+	cvz << numero;
+	snumero= cvz.str();
+	
+	return snumero;
+}
+
+/****************************************************************************/
---
+++ Equinoccio/Util/Util.h
@@ -0,0 +1,19 @@
+#ifndef UTIL_H_
+#define UTIL_H_
+
+#include <sstream>
+#include <string>
+
+/** 
+ * Clase con funciones comunes para el programa.
+ */
+/****************************************************************************/
+class Util {
+	
+	public:
+
+		static std::string intToString(uint32_t numero);
+};
+
+/****************************************************************************/
+#endif /*UTIL_H_*/
