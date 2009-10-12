514dd77349d66b5c23bc6e74559c5a7b85f8459c
nflabo@gmail.com <nflabo@gmail.com@c73f237e-9c17-11de-bb5f-cdebcd2927f5>
2009-10-12 21:26:10 +0000

    Parser de Php y pruebas.

---
+++ Equinoccio/Parsers/ParserPhp/Archivos_ejec.cpp
@@ -0,0 +1,20 @@
+#include "ParserPHP.h"
+int main(int argc, char** argv){/*Main de Prueba*/
+	std::string aux ="";
+	ParserPHP	 parser(500);
+	//parser.parsear("test",0);//Ok
+	//parser.parsear("test2",1);//OK
+	//parser.parsear("test3",0);//OK
+	//parser.parsear("test4",0);//Ok
+	//parser.parsear("test5",0);//Ok
+	//parser.parsear("test6",0);//Ok
+	//parser.parsear("test7",0);//Ok
+	//parser.parsear("test8",0);//OK
+	//parser.parsear("test9",0);//OK
+	//parser.parsear("test10",0);//OK
+	//parser.parsear("test11",0);//OK
+	//parser.parsear("test12",0);//OK
+	parser.parsear("test13",0);//OK
+	std::cout << "Finalizado" << std::endl;
+	return 0;
+}
---
+++ Equinoccio/Parsers/ParserPhp/ParserPHP.h
@@ -0,0 +1,27 @@
+#ifndef ParserC_
+#define ParserC_
+
+#include "../Parser.h"
+
+#define PATH_DUMP_C "dump_C_"
+#define PATH_STOP_WORD "stop_words_php"
+
+class ParserPHP: public Parser {
+
+	private:
+		void guardarEnDump(std::ofstream& dump, std::string palabra, uint32_t documento);
+		void Leer(const char *);
+		void separarLinea(std::string,short*);
+		void separarLineaRecursivo(std::string line,int campo,const char*,short);
+		static short isANumber(std::string);
+		static std::string limpiarComentarioSimple(std::string);
+		static std::string limpiarDobleComilla(std::string,const char);
+		static std::string devSubCadValCad(std::string cadorig,std::string coinc);
+		static std::string devSubCadValCar(std::string,char );
+		static std::string limpiarInclusiones(std::string aux);
+	public:
+		ParserPHP(uint32_t cantMaxReg);
+		virtual bool parsear(std::string nombre, uint32_t documento);
+	};
+
+#endif /*_*/
