ac9b85f0cb9b016279c8123dbdf49ff8cd6f77ab
vero13 <vero13@c73f237e-9c17-11de-bb5f-cdebcd2927f5>
2009-11-01 16:33:42 +0000

    mergeeeeeeeeeeeeee, ahi va bonitooooo

--- Equinoccio/Merge/Merge.h
+++ Equinoccio/Merge/Merge.h
@@ -1,8 +1,8 @@
 #ifndef __MERGE_H_INCLUDED__
 #define __MERGE_H_INCLUDED__
-#include "Arbol/RedBlackTree.h"
-#include "Registros/Registro.h"
-#include "Util/Util.h"
+#include "../Arbol/RedBlackTree.h"
+#include "../Registros/Registro.h"
+#include "../Util/Util.h"
 
 #include <fstream>
 #include <iostream>
