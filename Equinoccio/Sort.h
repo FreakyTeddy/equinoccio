455a1ba122ecdc2407b2c62816c3a66fb84373cf
gygounet@gmail.com <gygounet@gmail.com@c73f237e-9c17-11de-bb5f-cdebcd2927f5>
2009-11-01 15:45:07 +0000

    Sort Documentado

--- Equinoccio/Sort.h
+++ Equinoccio/Sort.h
@@ -9,13 +9,38 @@
 #include <iostream>
 #include <string>
 
+
+/**
+ * Clase que se encarga de ordenar
+ * el archivo mediante Replacement Selection
+ *
+ **/
 class Sorter{
 private:
      Sorter(){};
 public:
+	
+	/**
+	 * Funcion que se encarga de realizar
+	 * el sort del archivo dado utilizando
+	 * para ello, el metodo de Replacement
+	 * Selection, y valiendose de un arbol
+	 * rojo-negro para simplificar el manejo
+	 * de los registros en memoria.
+	 * 
+	 * @param nombre El nombre del archivo a ordenar
+	 * 		  nombreBaseSalida El nombre base de las 
+	 * 			particiones de salida, a esto se le
+	 * 			agregara un numero que indica el 
+	 * 			numero de cada particion.
+	 * 		  cantMax Es el tamaño de la memoria,
+	 * 			en este caso es la cantidad de registros
+	 * 			a agregar en el arbol.
+	 **/
      static int Sort(const std::string &nombreArchivo,			\
 		     const std::string &nombreBaseSalida,		\
 		     unsigned cantMax);
+
 };
 
 #endif //__SORT_H_INCLUDED__
