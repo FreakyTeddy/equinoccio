59dbdadbe4096c2016866e4a6094d106f5eee1c3
lu.teddy <lu.teddy@c73f237e-9c17-11de-bb5f-cdebcd2927f5>
2009-11-29 21:55:05 +0000

    mas interfaz

--- Equinoccio/Interfaz/Interfaz.cpp
+++ Equinoccio/Interfaz/Interfaz.cpp
@@ -4,10 +4,23 @@
 
 Interfaz::Interfaz() {
 	error=false;
+	activo=false;
 	try {
 		builder = Gtk::Builder::create_from_file(WINDOW_FILE);
 		main_window = 0;
 		builder->get_widget("window", main_window);//obtengo la ventana principal
+		about_window = 0;
+		builder->get_widget("about", about_window);
+		select_window = 0;
+		builder->get_widget("filechooserdialog", select_window);
+		select_window->add_button(Gtk::Stock::CANCEL, Gtk::RESPONSE_CANCEL);
+		select_window->add_button(Gtk::Stock::APPLY, Gtk::RESPONSE_OK);
+		builder->get_widget("button_search", button_buscar);
+		button_buscar->signal_clicked().connect(sigc::mem_fun(*this,
+					&Interfaz::on_button_buscar_clicked));
+		builder->get_widget("progressbar", progress_bar);
+		builder->get_widget("combobox", combo);
+		builder->get_widget("entry", entry_consulta);
 		cargarMenu();
 
 	} catch (Glib::FileError& ex1) {
@@ -30,13 +43,11 @@ Interfaz::~Interfaz() {}
 void Interfaz::cargarMenu() {
 	/* menu */
 	Glib::RefPtr<Gtk::ActionGroup> menu_archivo;
-//	Glib::RefPtr<Gtk::ActionGroup> menu_editar;
 	Glib::RefPtr<Gtk::ActionGroup> menu_ayuda;
 	Glib::RefPtr<Gtk::UIManager> menu_UIManager;
 	Gtk::Widget* menu;
 
 	menu_archivo = Gtk::ActionGroup::create();
-//	menu_editar = Gtk::ActionGroup::create();
 	menu_ayuda = Gtk::ActionGroup::create();
 
 	menu_archivo->add(Gtk::Action::create("FileMenu", "Archivo"));
@@ -45,12 +56,6 @@ void Interfaz::cargarMenu() {
 	menu_archivo->add(Gtk::Action::create("FileAdd", Gtk::Stock::OPEN,
 			"_Agregar Directorio", "Indexa el nuevo directorio"),
 			sigc::mem_fun(*this, &Interfaz::on_button_add_clicked));
-//
-//	menu_editar->add(Gtk::Action::create("EditMenu", "Editar"));
-//	menu_editar->add(Gtk::Action::create("EditPref", Gtk::Stock::PREFERENCES,
-//			"_Preferencias", "Preferencias de Configuracion"),
-//			sigc::mem_fun(*this, &Ventana::on_menu_preferences));
-
 	menu_ayuda->add(Gtk::Action::create("HelpMenu", "Ayuda"));
 	menu_ayuda->add(Gtk::Action::create("HelpAbout", Gtk::Stock::ABOUT,"A_cerca De", "Acerca de Equinoccio"),
 			sigc::mem_fun(*this, &Interfaz::on_menu_about));
@@ -59,7 +64,6 @@ void Interfaz::cargarMenu() {
 
 	menu_UIManager = Gtk::UIManager::create();
 	menu_UIManager->insert_action_group(menu_archivo);
-//	menu_UIManager->insert_action_group(menu_editar);
 	menu_UIManager->insert_action_group(menu_ayuda);
 
 	main_window->add_accel_group(menu_UIManager->get_accel_group());
@@ -97,16 +101,56 @@ void Interfaz::cargarMenu() {
 #endif //GLIBMM_EXCEPTIONS_ENABLED
 	menu = menu_UIManager->get_widget("/MenuBar");
 	Gtk::Box *box;
-	builder->get_widget("vbox2", box);
+	builder->get_widget("menubox", box);
 	box->pack_end(*menu, Gtk::PACK_SHRINK);
 }
 
-void Interfaz::on_menu_about(){}
-void Interfaz::on_menu_help(){}
+void Interfaz::on_menu_about() {
+	about_window->run();
+	about_window->hide();
+}
+
+void Interfaz::on_menu_help() {}
+
 void Interfaz::on_menu_quit() {
 	main_window->hide();
 }
-void Interfaz::on_button_add_clicked(){}
+
+void Interfaz::on_button_add_clicked() {
+	if (select_window->run() == Gtk::RESPONSE_OK);
+		std::cout<<select_window->get_filename()<<std::endl;
+	select_window->hide();
+}
+
+void Interfaz::on_button_buscar_clicked() {
+	if(!activo){
+		mostrarProgreso("Buscando..");
+		std::cout<<"Consulta: "<<entry_consulta->get_text()<<std::endl;//" - Catalogo:"<<combo.get_title()<<std::endl;
+		activo = true;
+	}
+	else {
+		detenerBarra(); //por ahora :P
+		activo = false;
+	}
+}
+
+void Interfaz::mostrarProgreso(Glib::ustring texto){
+	progress_bar->set_text(texto);
+	//actualiza la barra de progreso cada un determinado tiempo
+	id_activity = Glib::signal_timeout().connect(sigc::mem_fun(*this,
+		        &Interfaz::mover), 200 );
+}
+
+bool Interfaz::mover() {
+	progress_bar->pulse(); //mueve la barra de progreso
+	return true;
+}
+
+void Interfaz::detenerBarra() {
+	id_activity.disconnect();
+	progress_bar->set_text(" ");
+	progress_bar->set_fraction(0);
+}
 
 void Interfaz::run() {
 	if (!error) {
--- Equinoccio/Interfaz/Interfaz.h
+++ Equinoccio/Interfaz/Interfaz.h
@@ -8,7 +8,17 @@
 class Interfaz {
 private:
 	Gtk::Window *main_window; //ventana principal de la aplicacion
+	Gtk::AboutDialog *about_window; //ventana "acerca de"
 	Glib::RefPtr<Gtk::Builder> builder;//obtiene datos del archivo de la vista
+	Gtk::FileChooserDialog *select_window; //ventana de seleccion de directorio
+
+	Gtk::ProgressBar *progress_bar;//barra que muestra actividad
+	sigc::connection id_activity;//id del refresh de la barra
+	Gtk::Button *button_buscar;
+	Gtk::ComboBox *combo;
+	Gtk::Entry  *entry_consulta;
+
+	bool activo;
 	bool error;
 
 	void cargarMenu();
@@ -17,11 +27,15 @@ private:
 	void on_menu_help();
 	void on_menu_quit();
 	void on_button_add_clicked();
+	void on_button_buscar_clicked();
+	bool mover();
+	void mostrarProgreso(Glib::ustring texto);
 
 public:
 	Interfaz();
 	~Interfaz();
 	void run();
+	void detenerBarra();
 };
 
 #endif /* INTERFAZ_H_ */
