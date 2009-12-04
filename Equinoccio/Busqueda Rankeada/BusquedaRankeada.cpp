3b42a66d396a6d6608bdf0cfd65877fd2f4b3dde
gygounet@gmail.com <gygounet@gmail.com@c73f237e-9c17-11de-bb5f-cdebcd2927f5>
2009-12-04 18:37:57 +0000

    Para vero, carpetitas :P

---
+++ Equinoccio/Busqueda Rankeada/BusquedaRankeada.cpp
@@ -0,0 +1,2 @@
+#include "BusquedaRankeada.h"
+
---
+++ Equinoccio/Busqueda Rankeada/BusquedaRankeada.h
@@ -0,0 +1,13 @@
+#ifndef BUSQUEDARANKEADA_H_
+#define BUSQUEDARANKEADA_H_
+
+
+class BusquedaRankeada{
+	private:
+
+
+	public:
+
+};
+
+#endif /* BUSQUEDARANKEADA_H_ */
